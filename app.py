9a8889bba07a3cf8236d1bab51c59543af09b0f1
sushen <sushenbiswasaga@gmail.com>
2018-11-02 08:36:08 +0600

    Prepare other file foe heroku deployment

--- app.py
+++ app.py
@@ -1,69 +1,57 @@
-import os, sys
-from flask import Flask, request,render_template
-# from utils import wit_response
-# from pymessenger import Bot
-# from pprint import pprint
+import sys
+from flask import Flask, request
+from pprint import pprint
+from pymessenger import Bot
 
-app = Flask("Copping")
+app = Flask(__name__)
 
-FB_ACCESS_TOKEN = "EAAEwM5KUIVsBAGKZCeZBfaYPalWRQ0XxhKxJB7zZAN0UmSAz6PAJ70puNvAA2awndynoZCCXTNPLPitQF89cmitLXH5HhbQ4enX4WfeCxw5RZAReZCvh60JBW57BkCjwp5wCao321ZASYa6BU6xMj8DR9wLPnMPwjlo9x7ORuK3lQZDZD"
+FB_ACCESS_TOKEN = "EAAagZBYCBTiMBAB4g8uZBOiS2DwnjCSVTPEwBZA9wdJyfCbZBVeWg0FPMMjYoUDwZAZBHGQ8lAfbIZBmCmOaOsNLu2XJkVHwJIrUd6Bu48GN096mO3VKVDf6A6VtFJywHxJk7IRb3RqLiHRT2Ry8OAWbA39mUwm45onyaYjx2g1AgZDZD"
 bot = Bot(FB_ACCESS_TOKEN)
-
-VERIFICATION_TOKEN = "hello"
 
 
 @app.route('/', methods=['GET'])
 def verify():
-	# Webhook verification
-	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-		if not request.args.get("hub.verify_token") == "hello":
-			return "Verification token mismatch", 403
-		return request.args["hub.challenge"], 200
-	return "Hello world", 200
+    # Web hook verification
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "hello":
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world", 200
 
 
 @app.route('/', methods=['POST'])
 def webhook():
-	data = request.get_json()
-	log(data)
+    data = request.get_json()
+    log(data)
 
-	if data['object'] == 'page':
-		for entry in data['entry']:
-			for messaging_event in entry['messaging']:
+    # Necessary Code that extract json data facebook send
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            for messaging_event in entry['messaging']:
 
-				# IDs
-				sender_id = messaging_event['sender']['id']
-				recipient_id = messaging_event['recipient']['id']
+                # IDs
+                sender_id = messaging_event['sender']['id']
+                recipient_id = messaging_event['recipient']['id']
 
-				if messaging_event.get('message'):
-					# Extracting text message
-					if 'text' in messaging_event['message']:
-						messaging_text = messaging_event['message']['text']
-					else:
-						messaging_text = 'no text'
+                if messaging_event.get('message'):
+                    # Extracting text message
+                    if 'text' in messaging_event['message']:
+                        messaging_text = messaging_event['message']['text']
+                    else:
+                        messaging_text = 'no text'
 
-					response = None
+                    # Echo Bot
+                    response = messaging_text
+                    bot.send_text_message(sender_id, response)
 
-					entity, value = wit_response(messaging_text)
-					if entity == 'newstype':
-						response = "Ok, I will send you the {} news".format(str(value))
-					elif entity == 'location':
-						response = "Ok, so you live in {0}. Here are top headlines from {0}".format(str(value))
-
-					if response == None:
-						response = "Please Ask the question what is relevant to our product formalin remover"
-
-					bot.send_text_message(sender_id, response)
-
-	return "ok", 200
-
+    return "ok", 200
 
 
 def log(message):
-	print(message)
-	sys.stdout.flush()
-
+    # previously it was print now I just Use Petty Print
+    pprint(message)
+    sys.stdout.flush()
 
 
 if __name__ == "__main__":
-	app.run(port=80, use_reloader = True)
+    app.run(port=80, use_reloader=True)
